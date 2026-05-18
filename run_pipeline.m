a7a515e8ec9b89afec0c8107de42b08f669c7f4e
elthr <elother@kth.se>
2026-05-18 14:18:23 +0200

    Create run_pipeline.m

---
+++ run_pipeline.m
@@ -0,0 +1,10 @@
+clear; clc; close all;
+
+run('scripts/01_load_and_preprocess.m')
+run('scripts/02_descriptive_statistics.m')
+run('scripts/03_pca_analysis.m')
+run('scripts/04_kmeans_clustering.m')
+run('scripts/05_spectral_clustering.m')
+run('scripts/06_cluster_validation.m')
+run('scripts/07_biological_analysis.m')
+run('scripts/08_protein_modules.m')
